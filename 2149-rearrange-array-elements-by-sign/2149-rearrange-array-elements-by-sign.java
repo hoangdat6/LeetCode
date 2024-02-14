d654c687ed75756822cd1f9fd8cf66639e27e089
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-02-14 10:42:42 +0700

    Time: 3 ms (100%), Space: 76.9 MB (66.51%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,28 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positive = new ArrayList<>();
-        List<Integer> negative = new ArrayList<>();
-        List<Integer> relis = new ArrayList<>();
-        int n = nums.length;
-
-        int[] re = new int[n];
-        for(int i = 0; i < n; ++i){
-            if(nums[i] > 0){
-                positive.add(nums[i]);
-            }else{
-                negative.add(nums[i]);
+        int p = 0, n = 1;
+        int[] res = new int[nums.length];
+        
+        for(int i= 0;i<res.length;i++){
+            if(nums[i]>0){
+                res[p] = nums[i];
+                p += 2;
+            }
+            else{
+                res[n] = nums[i];
+                n += 2;
             }
         }
-
-        for(int i = 0; i < n / 2; ++i){
-            relis.add(positive.get(i));
-            relis.add(negative.get(i));
-        }
-
-        for(int i = 0; i < n; ++i){
-            re[i] = relis.get(i);
-        }
-
-        return re;
+        return res;
     }
 }
