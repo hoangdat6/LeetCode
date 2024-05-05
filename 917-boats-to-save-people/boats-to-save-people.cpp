764c55e1c0527b6e784a6282e34058cddd479259
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-05-05 10:11:30 +0700

    Time: 32 ms (99.84%) | Memory: 47.3 MB (9.11%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int limit) {
+        int re = 0;
+        
+        vector<int> buckets(limit + 1, 0);
+
+        for(int i : p){
+            buckets[i]++;
+        }
+
+        int left = 0, right = limit;
+
+        while(left <= right){
+            while(left <= right && buckets[left] <= 0) left++;
+            while(left <= right && buckets[right] <= 0) right--;
+
+            if(buckets[left] <= 0 && buckets[right] <= 0) break;
+            re++;
+
+            if(left + right <= limit){
+                buckets[left]--;
+            }
+            buckets[right]--;
+
+        }
+
+        return re;
+    }
+};
