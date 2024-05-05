7105243d9da63fe12a22f577b75963b810dde56c
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-05-05 10:15:00 +0700

    Time: 23 ms (100.00%) | Memory: 47.1 MB (9.11%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,11 +1,17 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
 
 class Solution {
 public:
     int numRescueBoats(vector<int>& p, int limit) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
         int re = 0;
+        
         vector<int> buckets(limit + 1, 0);
+
         for(int i : p){
             buckets[i]++;
         }
