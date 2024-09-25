402717a53c31ff5ef556b62ba02c402d934ee5f5
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-09-25 11:34:47 +0700

    Time: 5 ms (100.00%) | Memory: 45.3 MB (43.49%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,32 +1,26 @@
-class Solution {
-
+public final class Solution {
+    
     public static int minExtraChar(String s, String[] dictionary) {
-        Trie root = Trie.createTrie(dictionary);
+        Trie root = Trie.create(dictionary);
         int length = s.length();
         int[] table = new int[length + 1];
-
-        for(int i = length - 1; i >= 0; i--) {
-            table[i] = minExtraCharAt(s, dictionary, i, table, root);
-            System.out.print(table[i] + " ");
+        for (int index = length - 1; index >= 0; index--) {
+            table[index] = minExtraCharAt(s, index, length, root, table);
         }
-
         return table[0];
     }
 
-    private static int minExtraCharAt(String s, String[] dictionary, int i, int[] table, Trie node) {
-        int opt = 1 + table[i + 1];
-        int index = i;
-        int length = s.length();
-        while(index < length) {
+    private static int minExtraCharAt(String s, int offset, int length, Trie node, int[] table) {
+        int opt = 1 + table[offset + 1];
+        int index = offset;
+        while (index < length) {
             char c = s.charAt(index++);
-
             node = node.getChild(c);
-
-            if(node == null) {
+            if (node == null) {
                 break;
             }
-
-            if(!node.isWord()) {
+            
+            if (!node.isWord()) {
                 continue;
             }
 
@@ -38,42 +32,41 @@ class Solution {
 
 final class Trie {
 
-    private final static int CHAR_MASK = 0x1F;
-    private final Trie[] children = new Trie[27];
+    private static final int CHAR_MASK = 0x1F;
+
     private boolean isWord = false;
+    private final Trie[] children = new Trie[27];
 
-    private void addTrie(String s) {
-        Trie node = this;
-
-        for(char c : s.toCharArray()) {
-            int indexChar = c & CHAR_MASK;
-            Trie child = node.children[indexChar];
-
-            if(child == null) {
-                child = new Trie();
-                node.children[indexChar] = child;
-            }
-            node = child;
-        }
-
-        node.isWord = true;
-    }
-
-    static Trie createTrie(String[] directory) {
+    public static Trie create(String[] dictionary) {
         Trie root = new Trie();
-
-        for(String s : directory) {
-            root.addTrie(s);
+        for (String word : dictionary) {
+            root.addWord(word);
         }
-
         return root;
     }
 
-    Trie getChild(char c) {
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public Trie getChild(char c) {
         return children[c & CHAR_MASK];
     }
 
-    boolean isWord() {
-        return isWord;
+    private void addWord(String word) {
+        Trie node = this;
+        int length = word.length();
+        for (int index = 0; index < length; index++) {
+            int childIndex = word.charAt(index) & CHAR_MASK;
+            Trie child = node.children[childIndex];
+            if (child == null) {
+                child = new Trie();
+                node.children[childIndex] = child;
+            }
+            node = child;
+        }
+        node.isWord = true;
     }
+
+    private Trie() {}
 }
