24d30ae2b666da0498f2a7c19ef5555afc92e07c
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-09-25 15:05:00 +0700

    Time: 33 ms (93.25%) | Memory: 55.2 MB (84.43%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,47 +1,69 @@
-class Trie {
-    public Trie[] children;
-    public boolean first;
-    public boolean second;
-    public Trie() {
-        this.children = new Trie[10];
-    }
 
-    public void insert(int val, boolean first, boolean second) {
-        Trie copy = this;
-        String v = val + "";
-        for (char c : v.toCharArray()) {
-            int i = c - '0';
-            if (copy.children[i] == null) {
-                copy.children[i] = new Trie();
-            }
-            copy = copy.children[i];
-            if (first) copy.first = true;
-            if (second) copy.second = true;
-        }
-    }
-}
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Trie trie = new Trie();
-        for (int num : arr1) {
-            trie.insert(num, true, false);
-        }
-        for (int num : arr2) {
-            trie.insert(num, false, true);
-        }
+        Trie root = Trie.createTrie(arr1);
 
-        trie.first = true;
-        trie.second = true;
-        return maxSteps(trie);
+        int length2 = arr2.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < length2; i++) {
+            max = Math.max(max, longestPrefix(Integer.toString(arr2[i]), root)); 
+        }
+        return max;
     }
 
-    public int maxSteps(Trie trie) {
-        int steps = 0;
-        for (int i = 0;i < 10;i++) {
-            if (trie.children[i] != null && trie.children[i].first && trie.children[i].second) {
-                steps = Math.max(steps, maxSteps(trie.children[i]) + 1);
+     int longestPrefix(String word, Trie root) { 
+        int cnt = 0;
+        Trie current = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (current.getChild(word.charAt(i)) == null) {
+                break;
             }
+            cnt++;
+            current = current.getChild(word.charAt(i));
         }
-        return steps;
+        return cnt;
+    }
+}
+
+final class Trie {
+
+    private final static int CHAR_MASK = 0x1F;
+    private final Trie[] children = new Trie[10];
+    private boolean isWord = false;
+
+    private void addTrie(int num) {
+        Trie node = this;
+        String s = String.valueOf(num);
+
+        for(char c : s.toCharArray()) {
+            int indexChar = c - '0';
+            Trie child = node.children[indexChar];
+
+            if(child == null) {
+                child = new Trie();
+                node.children[indexChar] = child;
+            }
+            node = child;
+        }
+
+        node.isWord = true;
+    }
+
+    static Trie createTrie(int[] directory) {
+        Trie root = new Trie();
+
+        for(int s : directory) {
+            root.addTrie(s);
+        }
+
+        return root;
+    }
+
+    Trie getChild(char c) {
+        return children[c - '0'];
+    }
+
+    boolean isWord() {
+        return isWord;
     }
 }
