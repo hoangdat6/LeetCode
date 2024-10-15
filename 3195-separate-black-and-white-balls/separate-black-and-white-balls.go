a96dd9157db7bd928cdb57d93e6d497f0823c0b6
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-10-15 14:49:29 +0700

    Time: 9 ms (86.67%) | Memory: 6.3 MB (73.33%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.go
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.go
@@ -1,16 +1,19 @@
 func minimumSteps(s string) int64 {
     n := len(s)
-    var ans int64 = 0
-
-    cnt0 := 0
-
-    for i := n - 1; i >= 0; i-- {
-        if s[i] == '0' {
-            cnt0++
-        } else {
-            ans += int64(cnt0)
+    l, r := 0, n - 1
+    var ans int64
+    for l < r {
+        if s[l] == '0' {
+            l++
+            continue
         }
+        if s[r] == '1' {
+            r--
+            continue
+        }
+        ans += int64(r - l)
+        r--
+        l++
     }
-
     return ans
 }
