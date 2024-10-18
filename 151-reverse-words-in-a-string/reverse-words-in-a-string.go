7b37cd2e8755cb3f45024e2fe10b427682eabc58
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-10-18 08:12:19 +0700

    Time: 5 ms (23.47%) | Memory: 10.5 MB (5.07%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.go
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.go
@@ -1,26 +1,24 @@
-import "strings"
-
 func reverseWords(s string) string {
-    var builder strings.Builder
+    var ans string = ""
     len2 := len(s)
 
     isWord := false
     var start int
-
     for i := len2 - 1; i >= 0; i-- {
-        if !isWord && s[i] != ' ' {
-            start = i
-            isWord = true
+        if(!isWord && s[i] != ' ') {
+            start = i;
+            isWord = true;
         }
 
-        if isWord && (i == 0 || s[i-1] == ' ') {
-            if builder.Len() > 0 {
-                builder.WriteByte(' ') 
+        if isWord && (i == 0 || s[i - 1] == ' ') {
+            if len(ans) > 0 {
+                ans += " "
             }
-            builder.WriteString(s[i:start+1])
+
+            ans += s[i:start + 1]
             isWord = false
         }
     }
 
-    return builder.String()
+    return ans;
 }
