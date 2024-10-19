5f71de00ea1af198cd157b950e8cffa452306b19
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-10-19 23:55:29 +0700

    Time: 0 ms (100.00%) | Memory: 38.4 MB (56.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-          ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-        long long product = 1;
-        
-        int cnt0 = 0, cnt = 0;
-        for(int i = 0; i < nums.size(); ++i) {
-            if(nums[i] != 0) {
-                product *= nums[i];
-                cnt ++;
-            }else {
-                cnt0 ++;
-            }
+        vector<int> output(nums.size(), 1);
+
+        int left = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            output[i] *= left;
+            left *= nums[i];
         }
 
-        vector<int> ans(nums.size());
-
-        for(int i = 0; i < nums.size(); ++i) {
-            if(cnt0 >= 2 || cnt == 0) {
-                ans[i] = 0;
-                continue;
-            }else {
-                if(nums[i] == 0) {
-                    ans[i] = product;
-                }else {
-                    if(cnt0 != 0) {
-                        ans[i] = 0;
-                    }else {
-                        ans[i] = product / nums[i];
-                    }
-                }
-            }
+        int right = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
         }
 
-        return ans;
+        return output;        
     }
 };
