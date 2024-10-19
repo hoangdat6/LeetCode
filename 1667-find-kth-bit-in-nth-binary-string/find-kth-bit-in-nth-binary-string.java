540e39d5eb03dd6ed388fde81f0777af956f27f7
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-10-19 10:44:55 +0700

    Time: 50 ms (50.58%) | Memory: 54.4 MB (21.79%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,15 +1,15 @@
 class Solution {
     public char findKthBit(int n, int k) {
+        if(n == 1 && k == 1) return '0'; 
         StringBuilder sb = new StringBuilder("0");
 
-        for(int i = 2; i <= n; ++i) {
-            sb.append("1" + reverseOfInvert(sb.toString()));
-        }
-        return sb.toString().charAt(k - 1);
+        String s = getStringAtIndex(n, 2, sb);
+
+        return s.charAt(k - 1);
     }
 
-    private String reverseOfInvert(String s) {
-        char[] cs = s.toCharArray();
+    private String getStringAtIndex(int n, int index, StringBuilder sb) {
+        char[] cs = sb.toString().toCharArray();
 
         for(int i = 0; i < cs.length; ++i) {
             if(cs[i] == '1') {
@@ -19,12 +19,16 @@ class Solution {
             }
         }
 
-        for(int i = 0; i < cs.length / 2; i ++) {
+        for(int i = 0; i < cs.length / 2; i++) {
             char tmp = cs[i];
             cs[i] = cs[cs.length - i - 1];
             cs[cs.length - i - 1] = tmp;
         }
-
-        return String.valueOf(cs);
+        
+        sb.append("1" + String.valueOf(cs));
+        if(index == n) {
+            return sb.toString();
+        }
+        return getStringAtIndex(n, index + 1, sb);
     }  
 }
