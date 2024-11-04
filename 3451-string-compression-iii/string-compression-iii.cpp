bd03e1f18f67cb6b357ba147037f8be4f68c3719
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-04 11:50:36 +0700

    Time: 23 ms (76.07%) | Memory: 27.7 MB (83.84%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     string compressedString(string word) {
         if(word.size() == 0) return "";
-        string ans = "";
+        string ans;
 
         int cnt = 1;
         for(int i = 0; i < word.size() - 1; ++i) {
-            if(word[i] == word[i + 1]) ++cnt;
-            else {
+            if(word[i] == word[i + 1]) {
+                ++cnt;
+            } else {
                 if(cnt > 9) {
                     while(cnt >= 9) {
-                        ans += to_string(9) + word[i];
+                        ans.push_back('9');
+                        ans.push_back(word[i]);
                         cnt -= 9;
                     }
-                    if(cnt != 0) ans += to_string(cnt) + word[i];
-                }else {
-                    ans += to_string(cnt) + word[i];
+                    if(cnt != 0) {
+                        ans.push_back('0' + cnt);
+                        ans.push_back(word[i]);
+                    }
+                } else {
+                    ans.push_back('0' + cnt);
+                    ans.push_back(word[i]);
                 }
                 cnt = 1;
             }
@@ -23,13 +29,17 @@ public:
 
         if(cnt > 9) {
             while(cnt >= 9) {
-                ans += to_string(9) + word[word.size() - 1];
+                ans.push_back('9');
+                ans.push_back(word[word.size() - 1]);
                 cnt -= 9;
             }
-            if(cnt != 0) ans += to_string(cnt) + word[word.size() - 1];
-
-        }else {
-            ans += to_string(cnt) + word[word.size() - 1];
+            if(cnt != 0) {
+                ans.push_back('0' + cnt);
+                ans.push_back(word[word.size() - 1]);
+            }
+        } else {
+            ans.push_back('0' + cnt);
+            ans.push_back(word[word.size() - 1]);
         }
 
         return ans;
