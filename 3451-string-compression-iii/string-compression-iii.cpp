750fb263ecbef906f9681ff88675a893c2daddfe
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-04 11:45:34 +0700

    Time: 49 ms (34.19%) | Memory: 30.6 MB (6.28%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string temp;
-        char last=word[0];
-        int cnt=1;
-        for(int i=1; i<word.size(); i++){
-            if(last==word[i]){
-                if(cnt<9){
-                    ++cnt;
-                    continue;
-                }else{
-                    temp.push_back(cnt+'0');
-                    temp.push_back(last);
-                    cnt=1;
+        if(word.size() == 0) return "";
+        string ans = "";
+
+        int cnt = 1;
+        for(int i = 0; i < word.size() - 1; ++i) {
+            if(word[i] == word[i + 1]) ++cnt;
+            else {
+                if(cnt > 9) {
+                    while(cnt >= 9) {
+                        ans += to_string(9) + word[i];
+                        cnt -= 9;
+                    }
+                    if(cnt != 0) ans += to_string(cnt) + word[i];
+                }else {
+                    ans += to_string(cnt) + word[i];
                 }
-            }else{
-                temp.push_back(cnt+'0');
-                temp.push_back(last);
-                cnt=1;
-                last=word[i];
+                cnt = 1;
             }
         }
-        temp.push_back(cnt+'0');
-        temp.push_back(last);
-        return temp;
+
+        if(cnt > 9) {
+            while(cnt >= 9) {
+                ans += to_string(9) + word[word.size() - 1];
+                cnt -= 9;
+            }
+            if(cnt != 0) ans += to_string(cnt) + word[word.size() - 1];
+
+        }else {
+            ans += to_string(cnt) + word[word.size() - 1];
+        }
+
+        return ans;
     }
 };
