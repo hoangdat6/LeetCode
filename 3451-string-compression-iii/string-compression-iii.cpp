f949e4f53b287db2968ba7bf8c8038724a1155ed
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-04 11:43:42 +0700

    Time: 43 ms (45.30%) | Memory: 30.6 MB (6.28%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -6,23 +6,29 @@ public:
 
         int cnt = 1;
         for(int i = 0; i < word.size() - 1; ++i) {
-            if(word[i] == word[i + 1]) {
-                ++cnt;
-                if(cnt == 9) {
-                    ans += to_string(9) + word[i];
-                    cnt = 0;
-                }
-            }
+            if(word[i] == word[i + 1]) ++cnt;
             else {
-                if(cnt == 0) {
-                    cnt = 1;
-                    continue;
+                if(cnt > 9) {
+                    while(cnt >= 9) {
+                        ans += to_string(9) + word[i];
+                        cnt -= 9;
+                    }
+                    if(cnt != 0) ans += to_string(cnt) + word[i];
+                }else {
+                    ans += to_string(cnt) + word[i];
                 }
-                ans += to_string(cnt) + word[i];
                 cnt = 1;
             }
         }
-        if(cnt != 0) {
+
+        if(cnt > 9) {
+            while(cnt >= 9) {
+                ans += to_string(9) + word[word.size() - 1];
+                cnt -= 9;
+            }
+            if(cnt != 0) ans += to_string(cnt) + word[word.size() - 1];
+
+        }else {
             ans += to_string(cnt) + word[word.size() - 1];
         }
 
