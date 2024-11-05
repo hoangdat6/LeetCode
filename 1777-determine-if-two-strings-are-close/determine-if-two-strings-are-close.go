a0b31aa9c9272c20138d71fa4e47a8a1843bdcaa
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-05 22:46:51 +0700

    Time: 2 ms (90.00%) | Memory: 8.8 MB (20.23%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.go
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.go
@@ -1,28 +1,34 @@
 func closeStrings(word1 string, word2 string) bool {
-    freq1 := [26]int{}
-    freq2 := [26]int{}
-
-    for _, c := range word1 {
-        freq1[c - 'a']++
+    if len(word1) != len(word2) {
+        return false
     }
 
-    for _, c := range word2 {
-        freq2[c - 'a']++
+
+    nums1 := make([]int, 27)
+    nums2 := make([]int, 27)
+
+    for i := 0; i < len(word1); i++ {
+        nums1[word1[i]-'a']++
+        nums2[word2[i]-'a']++
     }
 
-    for i := 0; i < 26; i++ {
-        if (freq1[i] > 0) != (freq2[i] > 0) {
+    for i := 0; i < len(nums1); i++ {
+        if nums1[i] > 0 && nums2[i] > 0 {
+            continue
+        } else if nums1[i] == 0 && nums2[i] == 0 {
+            continue
+        } else {
             return false
         }
     }
 
-    sort.Ints(freq1[:])
-    sort.Ints(freq2[:])
-
-    for i := 0; i < 26; i++ {
-        if freq1[i] != freq2[i] {
-            return false;
-        } 
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+    for i := 0; i < len(nums1); i++ {
+        if nums1[i] != nums2[i] {
+            return false
+        }
     }
-    return true;
+    
+    return true
 }
