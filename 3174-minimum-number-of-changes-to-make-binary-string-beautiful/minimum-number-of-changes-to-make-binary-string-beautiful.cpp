d8d0c3a517dc1a7aaf96c892d77162ca48addf9f
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-05 22:12:31 +0700

    Time: 0 ms (100.00%) | Memory: 12.2 MB (67.52%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
 
         for(int i = 1; i < s.size(); i += 2) {
-            if(s[i - 1] != s[i]) ans ++; 
+            if(s[i - 1] != s[i]) ans++; 
         }
 
         return ans;
