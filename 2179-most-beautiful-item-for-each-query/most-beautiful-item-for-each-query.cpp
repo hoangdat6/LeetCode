afc6d163c6fb2437550b0f1181ff443c46fcd6a2
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-11-12 23:40:09 +0700

    Time: 1483 ms (5.74%) | Memory: 413 MB (5.07%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        int n = queries.size();
+        vector<int> ans(n);
+
+        sort(items.begin(), items.end(), [](vector<int> a, vector<int> b) {
+            return a[0] < b[0];
+        });
+
+        for(int i = 1; i < items.size(); ++i) {
+            items[i][1] = max(items[i - 1][1], items[i][1]);
+        }
+
+        int m = items.size();
+        for(int i = 0; i < n; ++i) {
+            int left = 0, right = m - 1;
+            int mid = (right - left) / 2;
+            int max = 0;
+            while(left <= right) {
+                mid = left + (right - left) / 2;
+                
+                if(items[mid][0] <= queries[i]) {
+                    max = items[mid][1];
+                    left = mid + 1; 
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            ans[i] = max;
+        }
+
+        return ans;
+    }
+};
