b2bbfcec0dc6bf29ecc0d9f3cb1e20fa81bd72ea
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-12-04 19:39:20 +0700

    Time: 7 ms (33.15%) | Memory: 17.2 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -4,7 +4,7 @@ public:
         int i1 = 0, i2 = 0;
         int len1 = str1.size(), len2 = str2.size();
         while(i1 < len1 && i2 < len2) {
-            if(str1[i1] == str2[i2] || (str1[i1] == 'z' && str2[i2] == 'a') || str1[i1] + 1 == str2[i2]) {
+            if(str1[i1] == str2[i2] || str1[i1] + 1 == str2[i2] || (str1[i1] == 'z' && str2[i2] == 'a')) {
                 i2++;
             }
             i1 ++;
