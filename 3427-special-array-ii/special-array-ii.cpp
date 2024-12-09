a21e45713c875c0672913d5c03bcd0104f29c7f6
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2024-12-10 00:50:24 +0700

    Time: 0 ms (100.00%) | Memory: 126.6 MB (63.34%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> a(n);
-        a[0] = 0;
+        vector<int> prefix(n);
         vector<bool> ans(queries.size());
+        prefix[0] = 0;
 
         for(int i = 1; i < n; ++i) {
-            a[i] = a[i - 1];
-            if(nums[i] % 2 == nums[i - 1] % 2) a[i] ++;
+            prefix[i] = prefix[i - 1];
+            if(nums[i] % 2 == nums[i - 1] % 2) prefix[i]++;
         }
 
         for(int i = 0; i < queries.size(); ++i) {
-            ans[i] = (a[queries[i][1]] - a[queries[i][0]]) == 0;
+            ans[i] = (prefix[queries[i][1]] - prefix[queries[i][0]]) == 0;
         }
 
         return ans;
