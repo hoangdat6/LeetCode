b4b020799323017b726a04bfb810e05171f8a630
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2025-01-02 12:34:17 +0700

    Time: 23 ms (95.00%) | Memory: 51.3 MB (25.00%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -7,13 +7,9 @@ class Solution(object):
         """
         vowels = {'a', 'e', 'i', 'o', 'u'}
 
-        n = len(words)
-        presum = [0] * (n + 1)
-        for i in range(n):
-            if words[i][0] in vowels and words[i][-1] in vowels:
-                presum[i + 1] = presum[i] + 1
-            else:
-                presum[i + 1] = presum[i]
+        presum = [0]
+        for word in words:
+            presum.append(presum[-1] + (1 if word[0] in vowels and word[-1] in vowels else 0))
         
         return [presum[end + 1] - presum[start] for start, end in queries]
 
