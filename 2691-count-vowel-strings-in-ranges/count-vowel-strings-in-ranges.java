1bd3bd35019a6932f77d6a7c7862d2db3fc64d71
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2025-01-02 12:21:23 +0700

    Time: 177 ms (5.01%) | Memory: 89.7 MB (11.39%) - LeetSync

---
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        int m = queries.length;
+        int[] presum =  new int[n + 1];
+        int[] ans =  new int[m];
+
+
+        if(isStartEndVowel(words[0])) presum[0] = 1;
+        System.out.print(presum[0] + " ");
+
+        for(int i = 1; i < n; ++i) {
+            if(isStartEndVowel(words[i])) {
+                presum[i] = presum[i - 1] + 1;
+            }else {
+                presum[i] = presum[i - 1];
+            }
+
+            System.out.print(presum[i] + " ");
+        }
+
+
+        for(int i = 0; i < m; ++i) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            
+            ans[i] = presum[end];
+            if(start - 1 >= 0) 
+                ans[i] -= presum[start - 1];
+        }
+        return ans;
+    }
+
+    private boolean isStartEndVowel(String s){
+        char start = s.charAt(0);
+        char end = s.charAt(s.length() - 1);
+        return (start == 'a' ||  start == 'e' || start == 'i' || start == 'o' || start == 'u')
+            && (end == 'a' || end == 'e' || end == 'i' ||  end == 'o' || end == 'u');
+    }
+}
