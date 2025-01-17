df20eff50b24c10a8a5e63cbd7fdca01f3d4345b
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2025-01-17 19:34:51 +0700

    Time: 2 ms (100.00%) | Memory: 63.5 MB (7.06%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-
-        int prev = 0;
+        int prev = 1;
         for(int b : derived) {
             prev ^= b;
         }
 
-        return prev == 0;
+        return prev == 1;
     }
 }
