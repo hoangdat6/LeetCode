272e2476a44fb9e6fcb7445ee58b559098454d76
Hoàng Đạt <118601984+hoangdat6@users.noreply.github.com>
2025-02-11 10:32:02 +0700

    Time: 19 ms (15.38%) | Memory: 17.8 MB (51.53%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -4,6 +4,7 @@ class Solution:
         stack = []
         for c in s:
             stack.append(c)
+            
             if len(stack) >= part_len and "".join(stack[-part_len:]) == part:
                 for _ in range(part_len):
                     stack.pop()
